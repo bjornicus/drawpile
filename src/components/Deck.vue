6b5020754b06ee327060eba1f3ab5ed5b9ee427b
Bjorn Hansen <bh@hoylu.com>
2017-11-09 21:37:40 -0800

    initial

---
+++ src/components/Deck.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "deck",
+  data() {
+    return {
+      msg: "Deck"
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
