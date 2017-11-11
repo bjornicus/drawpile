d885c533e420bfc5654c899194ed5f712f352955
Bjorn Hansen <bh@hoylu.com>
2017-11-10 17:50:04 -0800

    shuflle the deck

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -1,14 +1,52 @@
 <template>
-    <h1>{{ msg }}</h1>
+  <div>
+    <ul>
+      <li v-for="card in cards" :key="card.id">
+        {{ card.msg }}
+      </li>
+    </ul>
+    <button @click="shuffle">Shuffle</button>
+  </div>
 </template>
 
 <script>
+function shuffle(arrayToShuffle) {
+  let array = Array.from(arrayToShuffle);
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 export default {
   name: "deck",
   data() {
     return {
-      msg: "Deck"
+      cards: ["zero", "one", "two", "three", "four"].map((s, i) => ({
+        msg: s,
+        id: i
+      }))
     };
+  },
+  methods: {
+    shuffle() {
+      // debugger;
+      // console.log(shuffle([1, 2, 3]));
+      this.cards = shuffle(this.cards);
+    }
   }
 };
 </script>
