b53b70d94d91d631f4bdc8537e6747b42a1bfe3e
Bjorn Hansen <bh@hoylu.com>
2017-12-02 21:47:52 -0800

    add sw-precache

--- brunch-config.js
+++ brunch-config.js
@@ -15,6 +15,9 @@ module.exports = {
     //   extractCSS: true,
     //   out: '../public/styles/components.css'
     // },
+    swPrecache: {
+      autorequire: ['app/assets/index.html']
+    },
     babel: {
       plugins: ["transform-object-rest-spread"],
       presets: [['env', {
