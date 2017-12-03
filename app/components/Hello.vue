7cb7c9a387da0902af3b07500b1fe86789c09550
Bjorn Hansen <bh@hoylu.com>
2017-12-02 20:44:21 -0800

    basic brunch vue with auto-reload

---
+++ app/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  <p>can I haz auto reload?  I can!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hello",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App, bundled with Brunch! yay"
+    };
+  }
+};
+</script>
