2ea5cd6a8d40e6fb15000ce6b9351b8a3ff5893f
Bjorn Hansen <bh@hoylu.com>
2017-12-02 21:38:53 -0800

    vue-touch

--- app/main.js
+++ app/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import VueTouch from 'vue-touch'
 import App from './App'
 import router from './router'
 import "vueify/lib/insert-css" // required for .vue file <style> tags
 
 Vue.config.productionTip = false
 
+Vue.use(VueTouch)
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
